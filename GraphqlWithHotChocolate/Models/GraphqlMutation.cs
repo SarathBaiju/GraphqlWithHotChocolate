cb3277487e9ece6b508cb1e71c63fc1548bba8f2
Sarathbaiju <user@DESKTOP-HDI1QAS>
2022-02-13 12:40:34 +0530

    Add project files.

---
+++ GraphqlWithHotChocolate/Models/GraphqlMutation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphqlWithHotChocolate.Models
+{
+    public class GraphqlMutation
+    {
+        public string AddBook(Book book)
+        {
+            return book.Title;
+        }
+    }
+}
---
+++ GraphqlWithHotChocolate/Models/GrapqhlQuery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphqlWithHotChocolate.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+    }
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+    public class Query
+    {
+        public async Task<Book> GetBook()
+        {
+            return new Book
+            {
+                Title = "test",
+                Author = new Author
+                {
+                    Name = "tst author"
+                }
+            };
+        }
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return new List<Book>()
+            {
+                new Book
+                {
+                    Title = "test",
+                    Author = new Author
+                    {
+                        Name = "tst author"
+                    }
+                }
+            };
+        }
+    }
+}
+
